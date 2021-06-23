36f1fe7d5c70faeed2e8ab6c12013727aa8c2727
Lukasz J <lukas.jasinski3@gmail.com>
2021-06-23 15:32:22 +0100

    Set up string arrays

---
+++ message.js
@@ -0,0 +1,3 @@
+const starters = ["You will ", "You did ", "You should " ];
+const activities = ["play ", "train ", "work ", "cook ", "study"];
+const endings = ["good. Well done! ", "hard. Keep it going! ", "enough. Have a break"];
