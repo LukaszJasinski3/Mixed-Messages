e8f14afaaa090770aed2bfd5b587453fd135f810
Lukasz J <lukas.jasinski3@gmail.com>
2021-06-23 16:10:52 +0100

    Create function to get a random number

--- message.js
+++ message.js
@@ -2,13 +2,17 @@ const starters = ["You will", "You did", "You should" ];
 const activities = ["play", "train", "work", "cook", "study"];
 const endings = ["good. Well done!", "hard. Keep it going!", "enough. Have a break."];
 
+const generateRandomNumber = num => {
+    return Math.floor(Math.random() * num);
+};
+
 const mixedMessage = (arr1, arr2, arr3) => {
-    const startIndex = Math.floor(Math.random() * arr1.length);
-    const midIndex = Math.floor(Math.random() * arr2.length);
-    const endIndex = Math.floor(Math.random() * arr3.length);
+    const startIndex = generateRandomNumber(arr1.length);
+    const midIndex = generateRandomNumber(arr2.length);
+    const endIndex = generateRandomNumber(arr3.length);
 
     return `${arr1[startIndex]} ${arr2[midIndex]} ${arr3[endIndex]}`;
-}
+};
 
 console.log(mixedMessage(starters, activities, endings));
 
