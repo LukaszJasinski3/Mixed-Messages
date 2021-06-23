9a73678a7b53a6dd84f0259b3fa113794fa33a97
Lukasz J <lukas.jasinski3@gmail.com>
2021-06-23 15:47:45 +0100

    Write function to output mixed message

--- message.js
+++ message.js
@@ -1,3 +1,14 @@
-const starters = ["You will ", "You did ", "You should " ];
-const activities = ["play ", "train ", "work ", "cook ", "study"];
-const endings = ["good. Well done! ", "hard. Keep it going! ", "enough. Have a break"];
+const starters = ["You will", "You did", "You should" ];
+const activities = ["play", "train", "work", "cook", "study"];
+const endings = ["good. Well done!", "hard. Keep it going!", "enough. Have a break."];
+
+const mixedMessage = (arr1, arr2, arr3) => {
+    const startIndex = Math.floor(Math.random() * arr1.length);
+    const midIndex = Math.floor(Math.random() * arr2.length);
+    const endIndex = Math.floor(Math.random() * arr3.length);
+
+    return `${arr1[startIndex]} ${arr2[midIndex]} ${arr3[endIndex]}`;
+}
+
+console.log(mixedMessage(starters, activities, endings));
+
